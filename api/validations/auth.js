4f9fcd7203b489db5848eab6fd2ab24fd1a2a646
Yasin <yasin.142.tarakci@gmail.com>
2022-09-30 15:20:05 +0300

    update validation

--- api/validations/auth.js
+++ api/validations/auth.js
@@ -17,9 +17,26 @@ class AuthValidation {
 
     static async validateRegister(body) {
         const schema = Joi.object({
-            username: Joi.string().alphanum().min(3).required(),
-            email: Joi.string().email().required(),
-            password: Joi.string().required().min(6).pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+            username: Joi.string().alphanum().min(3).required().messages({
+                "string.alphanum": "Username must contain only letters and numbers",
+                "string.min": "Username must be at least 3 characters long",
+                "string.empty": "Username cannot be empty",
+                "any.required": "Username is required",
+            }),
+            email: Joi.string().email().required().messages({
+                "string.email": "Invalid email address",
+                "string.empty": "Email is required",
+                "any.required": "Email is required",
+            }),
+            password: Joi.string().required().min(8).pattern(
+                new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&._])[A-Za-z\\d@$!%*?&._]{8,}$")
+            ).messages({
+                "string.min": "Password must be at least 8 characters long",
+                "string.pattern": "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character",
+                "string.empty": "Password cannot be empty",
+                "string.required": "Password is required",
+                "any.required": "Password is required"
+            }),
         });
 
         const { error } = schema.validate(body);
